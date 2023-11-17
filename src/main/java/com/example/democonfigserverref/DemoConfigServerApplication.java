5d7164a7cfb66abb09786f92ac911d86c92a834c
Olga MaciaszekSharma <omaciaszeksh@vmware.com>
2023-11-17 16:03:54 +0100

    Try securing the Config Server with Spring Security and private repo git backend over SSH.

--- src/main/java/com/example/democonfigserverref/DemoConfigServerApplication.java
+++ src/main/java/com/example/democonfigserverref/DemoConfigServerApplication.java
@@ -3,6 +3,11 @@ package com.example.democonfigserverref;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @SpringBootApplication
 @EnableConfigServer
@@ -10,6 +15,15 @@ public class DemoConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoConfigServerApplication.class, args);
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		http.authorizeHttpRequests((authz) -> authz.anyRequest().authenticated())
+				.httpBasic(withDefaults());
+		http.csrf().ignoringRequestMatchers("/encrypt/**")
+				.ignoringRequestMatchers("/decrypt/**");
+		return http.build();
 	}
 
 }
